861a9c2f83b8bbe766c07ca94dafcbce666e8d35
kartikeya-bhatt <krtky020@gmail.com>
2021-07-23 01:54:09 -0700

    Fixed bug

--- src/main/java/com/workoutpal/backend/BackendApplication.java
+++ src/main/java/com/workoutpal/backend/BackendApplication.java
@@ -2,10 +2,8 @@ package com.workoutpal.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
