42de347b041cbfa257c9dcc620c2722201631fa6
kartikeya-bhatt <krtky020@gmail.com>
2021-07-23 01:53:05 -0700

    Attempting to fix bug related to cluster

--- src/main/java/com/workoutpal/backend/BackendApplication.java
+++ src/main/java/com/workoutpal/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.workoutpal.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class BackendApplication {
 
 	public static void main(String[] args) {
